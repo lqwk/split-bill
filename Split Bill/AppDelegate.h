fc3eca97df583e9fe6f12e3841028755985d5f2a
QINGWEI LAN <qingweilan@gmail.com>
2017-01-04 14:16:49 -0800

    Added basic files

---
+++ Split Bill/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Split Bill
+//
+//  Created by Qingwei Lan on 1/4/17.
+//  Copyright © 2017 Qingwei Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
