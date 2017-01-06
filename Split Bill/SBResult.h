59304c1929103d005304fcc827f4d4f7fdcc0d73
QINGWEI LAN <qingweilan@gmail.com>
2017-01-05 18:22:35 -0800

    Added SBResult for payment processing

---
+++ Split Bill/SBResult.h
@@ -0,0 +1,20 @@
+//
+//  SBResult.h
+//  Split Bill
+//
+//  Created by Qingwei Lan on 1/5/17.
+//  Copyright © 2017 Qingwei Lan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class SBPerson;
+@class SBMoney;
+
+@interface SBResult : NSObject
+
+@property (nonatomic, strong) SBPerson *lendee;
+@property (nonatomic, strong) SBPerson *lender;
+@property (nonatomic, strong) SBMoney *amount;
+
+@end
---
+++ Split Bill/SBResult.m
@@ -0,0 +1,13 @@
+//
+//  SBResult.m
+//  Split Bill
+//
+//  Created by Qingwei Lan on 1/5/17.
+//  Copyright © 2017 Qingwei Lan. All rights reserved.
+//
+
+#import "SBResult.h"
+
+@implementation SBResult
+
+@end
