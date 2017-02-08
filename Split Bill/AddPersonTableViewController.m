c09d8fb62fed9ec4b94fc0c95d506c3e220530f0
QINGWEI LAN <qingweilan@gmail.com>
2017-02-07 21:31:24 -0800

    Fixed spacing and colors

--- Split Bill/AddPersonTableViewController.m
+++ Split Bill/AddPersonTableViewController.m
@@ -102,6 +102,8 @@
         {
             StepperTableViewCell *temp = [tableView dequeueReusableCellWithIdentifier:@"AddPersonStepperCell" forIndexPath:indexPath];
             temp.weightLabel.textColor = [UIColor defaultColor];
+            temp.weightDescriptionLabel.textColor = [UIColor defaultColor];
+            temp.stepper.tintColor = [UIColor defaultColor];
             cell = temp;
             break;
         }
