4ad34ca7b21c4eab8cf5b28d24b58d7ad7af8af3
QINGWEI LAN <qingweilan@gmail.com>
2017-03-10 00:27:24 -0800

    Fixed compilation errors

--- Split Bill/Group+CoreDataProperties.m
+++ Split Bill/Group+CoreDataProperties.m
@@ -2,7 +2,7 @@
 //  Group+CoreDataProperties.m
 //  Split Bill
 //
-//  Created by Qingwei Lan on 3/1/17.
+//  Created by Qingwei Lan on 3/9/17.
 //  Copyright © 2017 Qingwei Lan. All rights reserved.
 //  This file was automatically generated and should not be edited.
 //
@@ -15,9 +15,9 @@
 	return [[NSFetchRequest alloc] initWithEntityName:@"Group"];
 }
 
+@dynamic currency;
 @dynamic name;
 @dynamic unique;
-@dynamic currency;
 @dynamic expenses;
 @dynamic people;
 
--- Split Bill/Payment+CoreDataProperties.h
+++ Split Bill/Payment+CoreDataProperties.h
@@ -2,7 +2,7 @@
 //  Payment+CoreDataProperties.h
 //  Split Bill
 //
-//  Created by Qingwei Lan on 1/13/17.
+//  Created by Qingwei Lan on 3/9/17.
 //  Copyright © 2017 Qingwei Lan. All rights reserved.
 //  This file was automatically generated and should not be edited.
 //
